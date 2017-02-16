4ae61ebf24f630e94cb39f39d06c412a0e65fa09
siruschen <670157389@qq.com>
2017-02-16 23:41:21 +0800

    初始化项目

---
+++ views/site/about.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+	<h1><?= Html::encode($this->title) ?></h1>
+
+	<p>
+		就是一个Yii自动生成的模板而已
+	</p>
+
+	<!-- <code><?= __FILE__ ?></code> -->
+</div>
