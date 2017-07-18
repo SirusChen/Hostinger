e8153da8a7ab616fe129df1b29a72cabb8b99499
sirus.chen <sirus.chen@vipshop.com>
2017-07-18 14:48:09 +0800

    [plcs#13822192566]打包发布　　　　　　　　　　　

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=vip_mmact',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=vip_mmact',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
